94763c95a394f3d8300ff2dd095b3f5e3f45286b
liuzongxin <liuzongxin946@163.com>
2024-05-03 02:59:17 +0800

    自定义业务异常

---
+++ spring-boot-init/src/main/java/com/lzx/springbootinit/common/exception/BizException.java
@@ -0,0 +1,38 @@
+package com.lzx.springbootinit.common.exception;
+
+import com.lzx.springbootinit.common.result.ErrorCode;
+import lombok.Data;
+
+/**
+ * 自定义业务异常
+ */
+@Data
+public class BizException extends RuntimeException {
+
+    private String code;
+
+    public BizException(String code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public BizException(String code, String message, Throwable cause) {
+        super(message, cause);
+        this.code = code;
+    }
+
+    public BizException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+    }
+
+    public BizException(ErrorCode errorCode, String message) {
+        super(message);
+        this.code = errorCode.getCode();
+    }
+
+    public BizException(ErrorCode errorCode, Throwable cause) {
+        super(errorCode.getMessage(), cause);
+        this.code = errorCode.getCode();
+    }
+}
